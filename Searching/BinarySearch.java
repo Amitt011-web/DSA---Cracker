e005bc470bc4ea52fb3935a84228e997c50d92db
Amit Kumar <www.amitkumar7565@gmail.com>
2026-06-03 10:06:24 +0530

    Implement binary search algorithm in BinarySearch.java

---
+++ Searching/BinarySearch.java
@@ -0,0 +1,15 @@
+class Saerching{
+
+    static int binarySearch(int[] arr, int target) {
+        int lo = 0, hi = arr.length - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+
+            if (arr[mid] == target) return mid;
+            else if (arr[mid] < target) lo = mid + 1;
+            else hi = mid - 1;
+            
+        }
+        return -1;
+    }
+}
