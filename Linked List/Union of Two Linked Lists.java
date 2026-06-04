edc45379dbf8c9ffa6e2226b87d39a9b81b65305
Amit Kumar <www.amitkumar7565@gmail.com>
2026-06-04 15:39:28 +0530

    Add union method for two linked lists
    Implement a method to create the union of two linked lists using a HashSet.

---
+++ Linked List/Union of Two Linked Lists.java
@@ -0,0 +1,14 @@
+class Solution {
+    public Node makeUnion(Node head1, Node head2) {
+        Node temp1 = head1, temp2 = head2;
+        HashSet<Integer> set =new Hashet<>();
+        while(temp1!=null){
+            set.add(temp1.data);
+            temp1 = temp1.next;
+        }
+        while(temp2!=null){
+            if(!con)
+        }
+        
+    }
+}
