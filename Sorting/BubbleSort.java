718870fb786d7dc8d3b58a16a56640ae7c6ee23b
Amit Kumar <www.amitkumar7565@gmail.com>
2026-06-04 17:26:45 +0530

    Bubble , Selection and Insertion Sort Technique

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -1,11 +1,45 @@
-  static void bubble (int[] arr){
-        for(int i = 0; i<arr.length-1; i++){
+class All_Sorting_Technique {
+    
+    static void bubble(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
             int swp = 0;
-            for(int j = 0; j<arr.length-1-i; j++){
-                if(arr[j]>arr[j+1]){
-                    swap(arr, j, j+1);
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j, j + 1);
                     swp++;
                 }
-            } if(swp == 0) break;
+            }
+            if (swp == 0) break;
         }
     }
+
+    static void selection(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIdx = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIdx]) {
+                    minIdx = j;
+                }
+            }
+            if (minIdx != i) {
+                swap(arr, i, minIdx);
+            }
+        }
+    }
+
+    static void insertion(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int j = i;
+            while (j > 0 && arr[j] < arr[j - 1]) {
+                swap(arr, j, j - 1);
+                j--;
+            }
+        }
+    }
+
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
