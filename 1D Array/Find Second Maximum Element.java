4b75194ba442189c389aaabdd8601bbee0e1463a
Amit Kumar <www.amitkumar7565@gmail.com>
2026-06-04 16:50:52 +0530

    Create Find Second Maximum Element.java

---
+++ 1D Array/Find Second Maximum Element.java
@@ -0,0 +1,15 @@
+ int secondMax(int[] arr) {
+        if (arr == null || arr.length < 2) return Integer.MIN_VALUE; 
+        
+        int max = Integer.MIN_VALUE, secMax = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                secMax = max;  ️
+                max = arr[i];  
+            }
+            else if (arr[i] > secMax && arr[i] != max) secMax = arr[i]; 
+            
+        }
+        return secMax;
+ }
