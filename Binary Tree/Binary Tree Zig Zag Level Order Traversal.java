fe83afb5afa6548585de9e1a426ec6c93484c12c
Amit Kumar <www.amitkumar7565@gmail.com>
2026-06-12 17:14:31 +0530

    Add zigzag level order traversal solution
    Implement zigzag level order traversal for binary tree.

---
+++ Binary Tree/Binary Tree Zig Zag Level Order Traversal.java
@@ -0,0 +1,57 @@
+/*
+Given the root of a binary tree, return the zigzag level order traversal of its nodes' values.
+(i.e., from left to right, then right to left for the next level and alternate between).
+
+Input: root = [3,9,20,null,null,15,7]
+Output: [[3],[20,9],[15,7]]
+Example 2:
+
+Input: root = [1]
+Output: [[1]]
+Example 3:
+
+Input: root = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 2000].
+-100 <= Node.val <= 100
+*/
+
+// SOLUTION //
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        
+        if (root == null)  return result;
+       
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        
+        boolean leftToRight = true;
+        
+        while (!q.isEmpty()) {
+            int size = q.size();
+            
+            List<Integer> currentLevel = new ArrayList<>();
+            
+            for (int i = 0; i < size; i++) {
+                TreeNode currentNode = q.remove();
+            
+                if (leftToRight)  currentLevel.add(currentNode.val);
+                else currentLevel.add(0, currentNode.val);
+          
+                if (currentNode.left != null) q.add(currentNode.left);
+                
+                if (currentNode.right != null) q.add(currentNode.right);
+                
+            }
+            result.add(currentLevel);
+            leftToRight = !leftToRight;
+        }
+        return result;
+        
+    }
+}
