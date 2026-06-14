9f2d16027459a41a3724a3c94422784b0389ebce
Amit Kumar <www.amitkumar7565@gmail.com>
2026-06-14 20:14:34 +0530

    Add transpose method for square matrix
    Implemented a method to compute the transpose of a square matrix.

---
+++ 2D Array/Transpose of Matrix.java
@@ -0,0 +1,43 @@
+/*
+You are given a square matrix of size n x n. Your task is to find the transpose of the given matrix.
+The transpose of a matrix is obtained by converting all the rows to columns and all the columns to rows.
+
+Examples :
+
+Input: mat[][] = [[1, 1, 1, 1],
+                [2, 2, 2, 2],
+                [3, 3, 3, 3],
+                [4, 4, 4, 4]]
+Output: [[1, 2, 3, 4],
+       [1, 2, 3, 4],
+       [1, 2, 3, 4],
+       [1, 2, 3, 4]]
+Explanation: Converting rows into columns and columns into rows.
+
+Input: mat[][] =  [[1, 2],
+                 [9, -2]]
+Output: [[1, 9],
+        [2, -2]]
+Explanation: Converting rows into columns and columns into rows.
+Constraints:
+1 ≤ n ≤ 103
+-109 ≤ mat[i][j] ≤109
+
+
+*/
+
+//        SOLUTION     //
+
+class Solution {
+    public ArrayList<ArrayList<Integer>> transpose(int[][] arr) {
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        for(int i = 0; i<arr[0].length; i++){
+            ArrayList<Integer> ans = new ArrayList<>();
+            for(int j = 0; j<arr.length; j++){
+               ans.add(arr[j][i]);
+            }
+            list.add(ans);
+        }
+        return list;
+    }
+}
