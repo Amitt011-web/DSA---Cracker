8823dc0eb043ffe6abdae63588cf90ea9083e03a
Amit Kumar <www.amitkumar7565@gmail.com>
2026-06-18 19:00:27 +0530

    Add angleClock method to calculate clock hand angle
    Implement a method to calculate the angle between clock hands.

---
+++ Mathematics/Angle Between Hands of a Clock.java
@@ -0,0 +1,59 @@
+/*
+Given two numbers, hour and minutes, return the smaller angle (in degrees) formed between the hour and the minute hand.
+
+Answers within 10-5 of the actual value will be accepted as correct.
+
+ 
+
+Example 1:
+
+
+Input: hour = 12, minutes = 30
+Output: 165
+Example 2:
+
+
+Input: hour = 3, minutes = 30
+Output: 75
+Example 3:
+
+
+Input: hour = 3, minutes = 15
+Output: 7.5
+ 
+
+Constraints:
+
+1 <= hour <= 12
+0 <= minutes <= 59
+ 
+
+*/
+
+
+//        SOOLUTION      //
+
+class Solution {
+    public double angleClock(int hour, int minute) {
+        if (hour == 12)  hour = 0;
+        if (hour > 12) hour = hour % 12;
+       
+        double hourAngle = (hour * 30) + (minute * 0.5);
+
+        double minuteAngle = minute * 6;
+
+        double angle = Math.abs(hourAngle - minuteAngle);
+
+        if (angle > 180)  angle = 360 - angle;
+        return angle;
+        
+
+    }
+}
+        
+       
+        
+        
+       
+
+
