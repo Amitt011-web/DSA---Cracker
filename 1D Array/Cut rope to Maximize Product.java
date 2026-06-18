b2d1a67c801058e52dc64f29b98136c15c60c315
Amit Kumar <www.amitkumar7565@gmail.com>
2026-06-18 16:40:14 +0530

    Create Cut rope to Maximize Product.java

---
+++ 1D Array/Cut rope to Maximize Product.java
@@ -0,0 +1,34 @@
+/*
+Given a rope of length n meters, cut it into multiple smaller ropes such that the product of their lengths is maximized. At least one cut is mandatory.
+
+Examples:
+
+Input: n = 2
+Output: 1
+Explanation: Since 1 cut is mandatory. Maximum obtainable product is 1 * 1 = 1.
+Input: n = 5
+Output: 6
+Explanation: Maximum obtainable product is 2 * 3 = 6.
+Constraints:
+2 ≤ n ≤ 58
+
+
+*/
+
+
+// SOLUTION  /  
+
+class Solution {
+    public int maxProduct(int n) {
+        if(n==2) return 1;
+        if(n==3) return 2;
+        
+        int product = 1;
+        while(n>4){
+            product = product * 3;
+            n = n-3;
+        }
+        product = product * n;
+        return product;
+    }
+}
