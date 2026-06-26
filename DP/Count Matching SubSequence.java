5a93e06a997b940aecc072b336ae4ec986140bb3
Amit Kumar <www.amitkumar7565@gmail.com>
2026-06-26 18:08:37 +0530

    Add function to count matching subsequences
    Implement a method to count subsequences of s1 that match s2.

---
+++ DP/Count Matching SubSequence.java
@@ -0,0 +1,41 @@
+/*
+Given two strings, s1 and s2, count the number of subsequences of string s1 equal to string s2.
+
+Return the total count modulo 1e9+7.
+
+Examples :
+
+Input: s1 = "geeksforgeeks", s2 = "gks"
+Output: 4
+Explanation: We can pick characters from s1 as a subsequence from indices [0, 3, 4], [0, 3, 12], [0, 11, 12] and [8, 11, 12]. So total 4 subsequences of 
+s1 that are equal to s2.
+
+Input: s1 = "problemoftheday", s2 = "geek"
+Output: 0
+Explanation: No subsequence of string s1 is equal to string s2.
+
+Constraints:
+1 ≤ s1.size(), s2.size() ≤ 103
+*/
+
+//      SOLUTION      //
+class Solution {
+    public static int countWays(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+        int mod = 1000000007;
+    
+        int[] dp = new int[m + 1];
+        dp[0] = 1; 
+    
+        for (int i = 1; i <= n; i++) {
+            for (int j = m; j >= 1; j--) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[j] = (dp[j] + dp[j - 1]) % mod;
+                }
+            }
+        }
+        
+        return dp[m];
+    }
+}
