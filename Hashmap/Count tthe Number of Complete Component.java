324a110f248791db7ee87cbb17172af5392b98ae
Amit Kumar <www.amitkumar7565@gmail.com>
2026-07-11 15:11:43 +0530

    Add solution to count complete components in graph
    Implement a solution to count complete connected components in a graph.

---
+++ Hashmap/Count tthe Number of Complete Component.java
@@ -0,0 +1,86 @@
+/**
+* You are given an integer n. There is an undirected graph with n vertices, numbered from 0 to n - 1. You are given a 2D integer array edges
+* where edges[i] = [ai, bi] denotes that there exists an undirected edge connecting vertices ai and bi.
+* Return the number of complete connected components of the graph.
+* A connected component is a subgraph of a graph in which there exists a path between any two vertices, and no vertex of the subgraph
+* shares an edge with a vertex outside of the subgraph.
+*
+* A connected component is said to be complete if there exists an edge between every pair of its vertices.
+*
+* Example 1:
+* Input: n = 6, edges = [[0,1],[0,2],[1,2],[3,4]]
+* Output: 3
+* Explanation: From the picture above, one can see that all of the components of this graph are complete.
+*
+* Example 2:
+* Input: n = 6, edges = [[0,1],[0,2],[1,2],[3,4],[3,5]]
+* Output: 1
+* Explanation: The component containing vertices 0, 1, and 2 is complete since there is an edge between every pair of two vertices.On the 
+* other hand, the component containing vertices 3, 4, and 5 is not complete since there is no edge between vertices 4 and 5. Thus, the number
+* of complete components in this graph is 1.
+* 
+*
+* Constraints:
+* 1 <= n <= 50
+* 0 <= edges.length <= n * (n - 1) / 2
+* edges[i].length == 2
+* 0 <= ai, bi <= n - 1
+* ai != bi
+* There are no repeated edges.
+* 
+**/
+
+
+      //      SOLUTION      //
+
+class Solution {
+    HashMap<Integer, List<Integer>> map;
+    boolean[] visited;
+
+    public int countCompleteComponents(int n, int[][] edges) {
+        map = new HashMap<>();
+        visited = new boolean[n];
+
+        for (int[] edge : edges) {
+            int src = edge[0], dest = edge[1];
+
+            if (!map.containsKey(src)) map.put(src, new ArrayList<>());
+            if (!map.containsKey(dest)) map.put(dest, new ArrayList<>());
+
+            map.get(src).add(dest);
+            map.get(dest).add(src);
+        }
+
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                int[] temp = dfs(i);
+                int ncount = temp[0], e = temp[1];
+                if ((ncount * (ncount - 1) == e) || ncount == 1)  ans++;
+            }
+        }
+        return ans;
+    }
+
+    int[] dfs(int node) {
+        int[] ans = new int[2];
+
+        visited[node] = true;
+        ans[0] += 1;
+        List<Integer> list = map.get(node);
+
+        if (list == null) return ans;
+
+        ans[1] += list.size();
+
+        for (int i = 0; i < list.size(); i++) {
+            if (visited[list.get(i)]) continue;
+            int[] t = dfs(list.get(i));
+
+            ans[0] += t[0];
+            ans[1] += t[1];
+        }
+        return ans;
+    }
+}
